315c8d6bc3f078584c7f155573030fe9111b3d41
Moises Corbellari <moisescorbellari1@outlook.com>
2024-09-11 19:27:12 -0300

    doing todo_list

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app.include_router(todo_list_router.router)
 if __name__ == "__main__":
     uvicorn.run("main:app", host='0.0.0.0', port=8000, reload=True, log_level="info")
     # uvicorn = starts an ASGI (Asynchronous Server Gateway Interface) web server.
-    # main:app = string indicates the entry point of the FastAPI application.
+    # main:app = string that indicates the entry point of the FastAPI application.
     # host='0.0.0.0': defines the IP address where the server will run.
     # reload=True: automatically reloads with each change saved in the application.
     # log_level="info": displays informative messages about what happens on the server.
