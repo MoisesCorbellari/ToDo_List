746c564c6154889025fc4181239e95f661141989
Moises Corbellari <moisescorbellari1@outlook.com>
2024-09-16 02:37:30 -0300

    changing import from main

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import uvicorn
+from uvicorn import run
 from fastapi import FastAPI
 from project_todo_list.routers import todo_list_client_router, todo_list_router
 from shared.exceptions import NotFound
@@ -15,7 +15,7 @@ app.include_router(todo_list_client_router.router)
 app.add_exception_handler(NotFound, not_found_exception_handler)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host='0.0.0.0', port=8000, reload=True, log_level="info")
+    run("main:app", host='0.0.0.0', port=8000, reload=True, log_level="info")
     # uvicorn = starts an ASGI (Asynchronous Server Gateway Interface) web server.
     # main:app = string that indicates the entry point of the FastAPI application.
     # host='0.0.0.0': defines the IP address where the server will run.
